4203c122d14fa8de67250dfa334f99055da3b26f
AlexiaMerolla <148439869+AlexiaMerolla@users.noreply.github.com>
2023-11-15 12:08:10 +0100

    Initial Commit

---
+++ MyReminders/MyRemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRemindersApp.swift
+//  MyReminders
+//
+//  Created by Alexia Merolla on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MyRemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
